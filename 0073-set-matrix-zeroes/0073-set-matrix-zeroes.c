5a5a81f0ca5d2a309f19e0f4a1f3053935076ed1
Ashirbad Pattnaik <pattnaikashirbad4@gmail.com>
2026-07-18 10:12:20 +0530

    Time: 0 ms (100%), Space: 20.4 MB (17.98%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.c
@@ -0,0 +1,53 @@
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    int firstRow = 0, firstCol = 0;
+
+    for (int i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            firstCol = 1;
+            break;
+        }
+    }
+
+    for (int j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            firstRow = 1;
+            break;
+        }
+    }
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            for (int j = 1; j < n; j++)
+                matrix[i][j] = 0;
+        }
+    }
+
+    for (int j = 1; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            for (int i = 1; i < m; i++)
+                matrix[i][j] = 0;
+        }
+    }
+
+    if (firstRow) {
+        for (int j = 0; j < n; j++)
+            matrix[0][j] = 0;
+    }
+
+    if (firstCol) {
+        for (int i = 0; i < m; i++)
+            matrix[i][0] = 0;
+    }
+}
