dfdea5d7cbc30d1662c42a0427627d1c3bb0cb29
Ashirbad Pattnaik <pattnaikashirbad4@gmail.com>
2026-07-18 10:11:48 +0530

    Time: 3 ms (76.66%), Space: 10.4 MB (43.14%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.c
@@ -0,0 +1,31 @@
+int minDistance(char* word1, char* word2) {
+    int m = strlen(word1);
+    int n = strlen(word2);
+
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = i;
+
+    for (int j = 0; j <= n; j++)
+        dp[0][j] = j;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                int insert = dp[i][j - 1];
+                int del = dp[i - 1][j];
+                int replace = dp[i - 1][j - 1];
+
+                int min = insert < del ? insert : del;
+                if (replace < min) min = replace;
+
+                dp[i][j] = 1 + min;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
